827dd0f70db99e274cb90fb6f8b141099f6e7066
pratapshouryasingh <147834096+pratapshouryasingh@users.noreply.github.com>
2025-08-30 15:36:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "2000MB" }));
 
 // -------------------- MongoDB --------------------
 const MONGODB_URI = process.env.MONGODB_URI;
