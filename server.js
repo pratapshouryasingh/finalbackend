d49572399a6959bf94cbbe9a5bf1c935b52f930a
pratapshouryasingh <147834096+pratapshouryasingh@users.noreply.github.com>
2025-09-23 22:09:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   "https://shippinglablecropper.vercel.app",
   "https://shippinglablecropper-git-main-pratapshouryasinghs-projects.vercel.app",
   "http://localhost:5173",
-  "https://shippingcropper.vercel.app"
+  "https://shippingcropper.vercel.app",
   "http://localhost:5000",
   "https://www.shippinglabelcrop.in",
   "https://shippinglabelcrop.in",
